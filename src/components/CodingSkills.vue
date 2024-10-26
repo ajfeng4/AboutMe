7bc2ffa0618b6046bf8dbc2942413bf9dbc10031
ajfeng4 <ajfeng@buffalo.edu>
2024-10-26 15:00:53 -0400

    Added skills component onto HomePage
    Added skills component

---
+++ src/components/CodingSkills.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-container class="text-center my-10">
+    <h2 class="display-2 font-weight-bold">My Skills</h2>
+    <v-row>
+      <v-col
+          v-for="(skill, index) in skills"
+          :key="index"
+          cols="6"
+          md="3"
+          class="d-flex justify-center"
+      >
+        <v-card outlined max-width="150" class="pa-4">
+          <v-icon :size="50">{{ skill.icon }}</v-icon>
+          <h3 class="mt-3">{{ skill.name }}</h3>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'CodingSkills',
+  data() {
+    return {
+      skills: [
+        { name: 'Git', icon: 'mdi-git' },
+        { name: 'JavaScript', icon: 'mdi-language-javascript' },
+        { name: 'Sass/SCSS', icon: 'mdi-sass' },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.v-card {
+  transition: transform 0.3s;
+}
+.v-card:hover {
+  transform: scale(1.05);
+}
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,6 @@
 <template>
   <v-container>
     <v-row align="center" justify="center" class="my-10">
-      <!-- Profile Information Container -->
       <v-col cols="12" md="6">
         <v-card flat>
           <h1 class="display-2 font-weight-bold mb-3">Hello, I'm Alex Feng</h1>
@@ -9,7 +8,7 @@
           <h3 class="subheading font-weight-bold mb-3">Brooklyn, New York</h3>
 
           <p class="body-1 mb-4">
-            A full time student taking a whole course load and working 40 hours a week. Passionate about front-end and mobile development,
+            A full-time student taking a full course load and working 40 hours a week. Passionate about front-end and mobile development,
             with experience in technical and non-technical environments. An experienced team player and seasoned leader.
           </p>
 
@@ -17,23 +16,27 @@
         </v-card>
       </v-col>
 
-      <!-- Image Placeholder Container -->
       <v-col cols="12" md="6" class="text-center">
         <v-card flat>
           <v-icon size="150" color="grey">mdi-account-circle</v-icon>
         </v-card>
       </v-col>
     </v-row>
+
+    <CodingSkills />
   </v-container>
 </template>
 
 <script>
 import SocialMedia from '../components/SocialMedia.vue';
+import CodingSkills from '../components/CodingSkills.vue';
 
 export default {
   name: 'HelloWorld',
   components: {
     SocialMedia,
+    CodingSkills,
   },
 };
 </script>
+
