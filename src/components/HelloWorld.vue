46ff2e29f5d0003f0d73b708ae2a7cabc0105c6a
ajfeng4 <ajfeng@buffalo.edu>
2024-10-26 14:32:13 -0400

    Seperated Social Media ledge into owner component
    Seperated social media ledger into new component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <v-container>
     <v-row align="center" justify="center" class="my-10">
+      <!-- Profile Information Container -->
       <v-col cols="12" md="6">
         <v-card flat>
           <h1 class="display-2 font-weight-bold mb-3">Hello, I'm Alex Feng</h1>
@@ -8,25 +9,11 @@
           <h3 class="subheading font-weight-bold mb-3">Brooklyn, New York</h3>
 
           <p class="body-1 mb-4">
-            A full time student taking an whole course load and working 40 hours a week. Passionate about front-end and mobile development,
-            with experience in technical and non-technical environments. A experienced team player and seasoned leader.
+            A full time student taking a whole course load and working 40 hours a week. Passionate about front-end and mobile development,
+            with experience in technical and non-technical environments. An experienced team player and seasoned leader.
           </p>
 
-          <!-- Social Icons -->
-          <v-row class="mb-4" align="center">
-            <v-btn icon>
-              <v-icon>mdi-github</v-icon>
-            </v-btn>
-            <v-btn icon>
-              <v-icon>mdi-twitter</v-icon>
-            </v-btn>
-            <v-btn icon>
-              <v-icon>mdi-facebook</v-icon>
-            </v-btn>
-            <v-btn icon>
-              <v-icon>mdi-linkedin</v-icon>
-            </v-btn>
-          </v-row>
+          <SocialMedia />
         </v-card>
       </v-col>
 
@@ -41,13 +28,12 @@
 </template>
 
 <script>
+import SocialMedia from '../components/SocialMedia.vue';
+
 export default {
   name: 'HelloWorld',
+  components: {
+    SocialMedia,
+  },
 };
 </script>
-
-<style scoped>
-.v-btn {
-  margin-right: 10px;
-}
-</style>
---
+++ src/components/SocialMedia.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-row class="mb-4" align="center">
+    <v-btn icon v-for="(icon, index) in icons" :key="index">
+      <v-icon>{{ icon }}</v-icon>
+    </v-btn>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'SocialMedia',
+  data() {
+    return {
+      icons: ['mdi-github', 'mdi-twitter', 'mdi-facebook', 'mdi-linkedin'],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.v-btn {
+  margin-right: 10px;
+}
+</style>
