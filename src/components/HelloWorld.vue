a9107265a72f23e97f058a54d23a044372f8b4fe
ajfeng4 <ajfeng@buffalo.edu>
2024-11-10 17:06:38 -0500

    Resolved issue with navbar
    Resolved problem with navbar

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,25 +1,27 @@
 <template>
   <v-container>
-    <v-row align="center" justify="center" class="my-10">
-      <v-col cols="12" md="6">
-        <v-card flat>
-          <h1 class="display-2 font-weight-bold mb-3">Hello, I'm Alex Feng</h1>
-          <h2 class="headline font-weight-bold">Software Engineer</h2>
-          <h3 class="subheading font-weight-bold mb-3">Brooklyn, New York</h3>
-          <p class="body-1 mb-4">
-            A full-time student taking a full course load and working 40 hours a week. Passionate about front-end and mobile development,
-            with experience in technical and non-technical environments. An experienced team player and seasoned leader.
-          </p>
-          <SocialMedia />
-        </v-card>
-      </v-col>
+    <div id="about">
+      <v-row align="center" justify="center" class="my-10">
+        <v-col cols="12" md="6">
+          <v-card flat>
+            <h1 class="display-2 font-weight-bold mb-3">Hello, I'm Alex Feng</h1>
+            <h2 class="headline font-weight-bold">Software Engineer</h2>
+            <h3 class="subheading font-weight-bold mb-3">Brooklyn, New York</h3>
+            <p class="body-1 mb-4">
+              A full-time student taking a full course load and working 40 hours a week. Passionate about front-end and mobile development,
+              with experience in technical and non-technical environments. An experienced team player and seasoned leader.
+            </p>
+            <SocialMedia />
+          </v-card>
+        </v-col>
 
-      <v-col cols="12" md="6" class="text-center">
-        <v-card flat>
-          <v-icon size="150" color="grey">mdi-account-circle</v-icon>
-        </v-card>
-      </v-col>
-    </v-row>
+        <v-col cols="12" md="6" class="text-center">
+          <v-card flat>
+            <v-icon size="150" color="grey">mdi-account-circle</v-icon>
+          </v-card>
+        </v-col>
+      </v-row>
+    </div>
 
     <v-row>
       <v-col cols="12">
@@ -27,28 +29,43 @@
       </v-col>
     </v-row>
 
-    <CodingSkills />
-    <v-row class="my-12">
-      <v-col cols="12">
-        <WorkExperience />
-      </v-col>
-    </v-row>
-    <v-row class="my-12">
-      <v-col cols="12">
-        <Education />
-      </v-col>
-    </v-row>
-    <v-row class="my-12">
-      <v-col cols="12">
-        <Projects />
-      </v-col>
-    </v-row>
+    <div id="skills">
+      <CodingSkills />
+    </div>
+
+    <div id="education">
+      <v-row class="my-12">
+        <v-col cols="12">
+          <Education />
+        </v-col>
+      </v-row>
+    </div>
+
+    <div id="experience">
+      <v-row class="my-12">
+        <v-col cols="12">
+          <WorkExperience />
+        </v-col>
+      </v-row>
+    </div>
+
+    <div id="projects">
+      <v-row class="my-12">
+        <v-col cols="12">
+          <Projects />
+        </v-col>
+      </v-row>
+    </div>
+
     <v-row class="my-12">
       <v-col cols="12">
         <GitHubStats />
       </v-col>
     </v-row>
-    <ContactMe />
+
+    <div id="contact">
+      <ContactMe />
+    </div>
   </v-container>
 </template>
 
