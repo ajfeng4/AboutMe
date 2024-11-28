efa8c2afa0af306ca27c3a758173a611fccc2cee
ajfeng4 <ajfeng@buffalo.edu>
2024-11-28 18:04:59 -0500

    Scrollbar added to education
    Scrollbar added to education

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -3,50 +3,52 @@
     <h2 class="display-2 font-weight-bold">
       My <span style="font-weight: 900;">Education</span>
     </h2>
-    <v-row justify="center">
-      <v-col cols="12" md="10">
-        <v-card
-            v-for="(education, index) in educations"
-            :key="index"
-            class="education-card my-5"
-            outlined
-        >
-          <v-row class="align-start">
-            <v-col cols="2" class="text-left">
-              <v-img :src="education.image" max-width="200"></v-img>
-            </v-col>
-            <v-col cols="10" class="text-left">
-              <div class="d-flex justify-space-between align-start">
-                <div class="education-details">
-                  <h3 class="education-major ml-2">
-                    {{ education.major }}
-                  </h3>
-                  <span class="education-school">
-                    {{ education.school }}
+    <div class="scrollable-education">
+      <v-row justify="center">
+        <v-col cols="12" md="10">
+          <v-card
+              v-for="(education, index) in educations"
+              :key="index"
+              class="education-card my-5"
+              outlined
+          >
+            <v-row class="align-start">
+              <v-col cols="2" class="text-left">
+                <v-img :src="education.image" max-width="200"></v-img>
+              </v-col>
+              <v-col cols="10" class="text-left">
+                <div class="d-flex justify-space-between align-start">
+                  <div class="education-details">
+                    <h3 class="education-major ml-2">
+                      {{ education.major }}
+                    </h3>
+                    <span class="education-school">
+                      {{ education.school }}
+                    </span>
+                  </div>
+                  <div class="education-date">
+                    {{ education.date }}
+                  </div>
+                </div>
+                <div class="d-flex align-center mt-2">
+                  <v-icon
+                      color="grey darken-1"
+                      size="20"
+                      class="mr-1"
+                  >mdi-map-marker</v-icon>
+                  <span class="education-location">
+                    {{ education.location }}
                   </span>
                 </div>
-                <div class="education-date">
-                  {{ education.date }}
-                </div>
-              </div>
-              <div class="d-flex align-center mt-2">
-                <v-icon
-                    color="grey darken-1"
-                    size="20"
-                    class="mr-1"
-                >mdi-map-marker</v-icon>
-                <span class="education-location">
-                  {{ education.location }}
-                </span>
-              </div>
-              <p class="education-description mt-3">
-                {{ education.description }}
-              </p>
-            </v-col>
-          </v-row>
-        </v-card>
-      </v-col>
-    </v-row>
+                <p class="education-description mt-3">
+                  {{ education.description }}
+                </p>
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-col>
+      </v-row>
+    </div>
   </v-container>
 </template>
 
@@ -64,7 +66,7 @@ export default {
           school: 'University at Buffalo, State University of New York',
           location: 'Buffalo, New York',
           date: 'Jan 2022 - Present',
-          description: 'Description',
+          description: 'Relevant Courses:',
           image: NorthCampus,
           icon: 'mdi-school',
           iconColor: 'purple',
@@ -77,7 +79,7 @@ export default {
           school: 'University at Buffalo, State University of New York',
           location: 'Buffalo, New York',
           date: 'Jan 2022 - Present',
-          description: 'Description',
+          description: 'Relevant Courses:',
           image: NorthCampus,
           icon: 'mdi-school',
           iconColor: 'green',
@@ -93,18 +95,12 @@ export default {
 .education-card {
   padding: 24px;
   background-color: #fff;
-  width: 600px;
-  height: 200px;
-  margin-left: -75px;
+  width: 120%;
+  height: 150px;
+  margin-left: -50px;
   box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
   border-radius: 12px;
-  transition: transform 0.3s, background-color 0.3s;
   text-align: left;
-}
-
-.education-card:hover {
-  transform: scale(1.02);
-  background-color: #f9f9f9;
 }
 
 .education-major {
@@ -135,7 +131,7 @@ export default {
 }
 
 .education-description {
-  font-size: 16px;
+  font-size: 12px;
   color: #424242;
   margin-top: 8px;
   word-wrap: break-word;
@@ -155,5 +151,35 @@ export default {
 
 .v-img {
   margin-right: auto;
+}
+
+.scrollable-education {
+  max-height: 420px;
+  overflow-y: auto;
+  overflow-x: hidden;
+}
+
+/* Scrollbar Styles */
+.scrollable-education::-webkit-scrollbar {
+  width: 8px;
+}
+
+.scrollable-education::-webkit-scrollbar-track {
+  background: #f1f1f1;
+  border-radius: 4px;
+}
+
+.scrollable-education::-webkit-scrollbar-thumb {
+  background: #c1c1c1;
+  border-radius: 4px;
+}
+
+.scrollable-education::-webkit-scrollbar-thumb:hover {
+  background: #a8a8a8;
+}
+
+.scrollable-education {
+  scrollbar-width: thin;
+  scrollbar-color: #c1c1c1 #f1f1f1;
 }
 </style>
