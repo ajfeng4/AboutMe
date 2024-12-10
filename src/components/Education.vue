8b557111230ea249c732d8343874f13085f8e4fd
ajfeng4 <ajfeng@buffalo.edu>
2024-12-10 16:55:02 -0500

    Fixed education component
    Education component changed

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -10,20 +10,17 @@
             :key="index"
             class="education-card my-10"
         >
+          <div class="education-header"></div>
           <v-row class="align-start">
             <v-col cols="2" class="text-left logo-col">
-              <v-img :src="education.image" max-width="120"></v-img>
+              <v-img :src="education.image" max-width="120" class="education-logo"></v-img>
             </v-col>
             <v-col cols="10" class="text-left">
               <div class="education-school-name">{{ education.school }}</div>
               <div class="education-major-title">{{ education.major }}</div>
               <div class="education-date">{{ education.date }}</div>
               <div class="education-location d-flex align-center mt-2">
-                <v-icon
-                    color="grey darken-1"
-                    size="20"
-                    class="mr-1"
-                >mdi-map-marker</v-icon>
+                <v-icon color="grey darken-1" size="20" class="mr-1">mdi-map-marker</v-icon>
                 {{ education.location }}
               </div>
               <div class="education-description mt-3">
@@ -38,7 +35,7 @@
 </template>
 
 <script>
-import NorthCampus from "@/assets/northcampus.png";
+import NorthCampus from "@/assets/buffalo.png";
 
 export default {
   name: 'Education',
@@ -78,7 +75,8 @@ export default {
 
 <style scoped>
 .education-card {
-  padding: 40px 0;
+  position: relative;
+  padding: 120px 0 40px;
   background-color: #fff;
   border-bottom: 1px solid #e0e0e0;
   box-shadow: none;
@@ -86,10 +84,32 @@ export default {
   text-align: left;
 }
 
+.education-header {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 120px;
+  background: url('../assets/campus.png') center center/cover no-repeat;
+  border-top-left-radius: 12px;
+  border-top-right-radius: 12px;
+}
+
 .logo-col {
+  position: relative;
+  z-index: 1;
   display: flex;
   align-items: flex-start;
   justify-content: flex-start;
+  margin-top: -60px;
+  margin-left: 5px;
+}
+
+.education-logo {
+  border-radius: 50%;
+  border: 1px solid #fff;
+  background-color: #fff;
+  box-shadow: 0 0 10px rgba(0,0,0,0.2);
 }
 
 .education-school-name {
